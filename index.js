eb74822f40ea39b28fd379b114313f589d6a46f8
Rishu Anand <rishuanandchaudhary@yahoo.com>
2018-07-23 06:06:06 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var recipes = {}; // empty recipes object 
+var recipes = {}; // empty recipes object
 
 function updateObjectWithKeyAndValue (object, key, value) {
   return (Object.assign({}, object, {[key]: value}));
